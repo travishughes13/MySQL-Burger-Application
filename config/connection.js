ef15fb2e2ca8680725eb2da1cb8b20530f76be7b
TREX Hughes <TREX@Traviss-MacBook-Pro-2.local>
2017-06-11 20:58:22 -0500

    some updates

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,6 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
-        root: 3000,
         host: 'localhost',
         user: 'root',
         password: '',
